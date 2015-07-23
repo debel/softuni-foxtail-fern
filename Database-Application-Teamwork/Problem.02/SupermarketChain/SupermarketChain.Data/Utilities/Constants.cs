9e751567d88b71180a53bc012f9d2b05545354b5
DanielJelev <subleden@abv.bg>
2015-07-23 15:30:27 +0300

    Delete trash

--- Database-Application-Teamwork/Problem.02/SupermarketChain/SupermarketChain.Data/Utilities/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SupermarketChain.Data.Utilities
-{
-    public static class Constants
-    {
-        public const string SampleDataPath = @"C:\Users\user\Desktop\softuni-foxtail-fern.git\trunk\Database-Application-Teamwork\Problem.02\SupermarketChain\SampleData\";
-    }
-}
