ae85f76c3a13e17e17298df99e7da2d378457733
Joro <infreezer@gmail.com>
2015-07-26 01:16:17 +0300

    MongoDb connected and method for inserting sales reports added

--- Database-Application-Teamwork/SupermarketChain/05. ExportToJSON/ExportToJsonMain.cs
+++ Database-Application-Teamwork/SupermarketChain/05. ExportToJSON/ExportToJsonMain.cs
@@ -6,10 +6,36 @@
     using SupermarketChain.Data.Data;
     using SupermarketChain.Data.Utilities;
     using System.IO;
+    using MongoDB.Bson;
+    using MongoDB.Bson.IO;
+    using MongoDB.Bson.Serialization;
+    using MongoDB.Bson.Serialization.Serializers;
+    using MongoDB.Driver;
 
     class ExportToJsonMain
     {
-        static void Main(string[] args)
+        static void Main()
+        {
+            GenerateJsonReports();
+        }
+
+        private static IMongoCollection<BsonDocument> GetDatabaseCollection()
+        {
+            var client = new MongoClient(Constants.MongoDbDatabaseHost);
+            var database = client.GetDatabase(Constants.MongoDbDatabaseName);
+            var collection = database.GetCollection<BsonDocument>("SalesByProductReports");
+            return collection;
+        }
+
+        private static void SaveDataInMongoDb(string productJson)
+        {
+            var reader = new JsonReader(productJson);
+            var context = BsonDeserializationContext.CreateRoot(reader);
+            BsonDocument doc = BsonDocumentSerializer.Instance.Deserialize(context);
+            GetDatabaseCollection().InsertOneAsync(doc).Wait();
+        }
+
+        private static void GenerateJsonReports()
         {
             Console.WriteLine("Enter startDate and endDate separated by space in format(yyyy-mm-dd):");
             string input = Console.ReadLine();
@@ -18,9 +44,10 @@
                 string[] dates = input.Split(' ');
                 DateTime startDate = DateTime.Parse(dates[0]);
                 DateTime endDate = DateTime.Parse(dates[1]);
-				if(startDate > endDate){
-					throw new ArgumentException("StartDate must be before endDate");
-				}
+                if (startDate > endDate)
+                {
+                    throw new ArgumentException("StartDate must be before endDate");
+                }
                 var myssqlData = new SupermarketChainMssqlData();
                 var jsSerializer = new JavaScriptSerializer();
                 var sales = myssqlData.Sales
@@ -39,11 +66,13 @@
 
                 foreach (var sale in sales)
                 {
-                    var productsJson = jsSerializer.Serialize(sale);
-                    Console.WriteLine(productsJson);
+                    var productJson = jsSerializer.Serialize(sale);
+
+                    SaveDataInMongoDb(productJson);
+
                     if (!File.Exists(Constants.jsonReportPath + sale.productId + ".json"))
                     {
-                        File.WriteAllText(Constants.jsonReportPath + sale.productId + ".json", productsJson);
+                        File.WriteAllText(Constants.jsonReportPath + sale.productId + ".json", productJson);
                     }
                 }
             }
--- Database-Application-Teamwork/SupermarketChain/SupermarketChain.Data/Utilities/Constants.cs
+++ Database-Application-Teamwork/SupermarketChain/SupermarketChain.Data/Utilities/Constants.cs
@@ -4,6 +4,7 @@
     {
         public const string SampleDataPath = @"C:\Users\user\Desktop\softuni-foxtail-fern.git\trunk\Database-Application-Teamwork\SupermarketChain\SampleData\";
         public const string jsonReportPath = @"C:\Users\user\Desktop\softuni-foxtail-fern.git\trunk\Database-Application-Teamwork\SupermarketChain\05. ExportToJSON\JSONReports\";
-
+        public const string MongoDbDatabaseHost = "mongodb://localhost:27017";
+        public const string MongoDbDatabaseName = "SupermarketChain";
     }
 }
