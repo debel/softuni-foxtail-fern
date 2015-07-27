f3f79e656e3699da5db7a4cc12e9e8f3a37f6e7d
Rositsa <qrostna1@abv.bg>
2015-07-27 16:03:03 +0300

    Added LoadExpenseData from XML

--- Database-Application-Teamwork/SupermarketChain/SupermarketChain.Data/Utilities/Constants.cs
+++ Database-Application-Teamwork/SupermarketChain/SupermarketChain.Data/Utilities/Constants.cs
@@ -1,12 +1,15 @@
 ﻿namespace SupermarketChain.Data.Utilities
 {
+    using System.IO;
+
     public static class Constants
     {
         public const string SampleDataPath = @"C:\Users\Jinjaar-pc\Desktop\softuni-foxtail-fern\Database-Application-Teamwork\SupermarketChain\SampleData\";
         public const string jsonReportPath = @"C:\Users\Jinjaar-pc\Desktop\softuni-foxtail-fern\Database-Application-Teamwork\SupermarketChain\05. ExportToJSON\JSONReports\";
         public const string MongoDbDatabaseHost = "mongodb://localhost:27017";
-       public const string MongoDbDatabaseName = "SupermarketChain";
+        public const string MongoDbDatabaseName = "SupermarketChain";
         public const string PdfReportPath = @"C:\Users\Jinjaar-pc\Desktop\softuni-foxtail-fern\Database-Application-Teamwork\SupermarketChain\03. ExportToPdf\PdfReport\";
         public const string SalesImportPath = @"C:\Users\Jinjaar-pc\Desktop\softuni-foxtail-fern\Database-Application-Teamwork\SupermarketChain\02. ZipManager\Import\Sample-Sales-Reports.zip";
+        public const string XmlExpensesPath = @"C:\Users\user\Desktop\softuni-foxtail-fern\Database-Application-Teamwork\SupermarketChain\06. LoadExpenseDataFromXml\Sample-Vendor-Expenses.xml";
     }
 }
