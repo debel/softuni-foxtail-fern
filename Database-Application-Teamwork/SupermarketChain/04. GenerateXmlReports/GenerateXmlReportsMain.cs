fc439a0fde57e036a3db272b582197d7951c7f2c
Joro <infreezer@gmail.com>
2015-07-27 17:32:30 +0300

    Generate XML Sales by Vendor Report Added

---
+++ Database-Application-Teamwork/SupermarketChain/04. GenerateXmlReports/GenerateXmlReportsMain.cs
@@ -0,0 +1,10 @@
+﻿namespace _04.GenerateXmlReports
+{
+    class GenerateXmlReportsMain
+    {
+        static void Main()
+        {
+            GenerateXmlReports.Generate();
+        }
+    }
+}
--- Database-Application-Teamwork/SupermarketChain/SupermarketChain.Data/Utilities/Constants.cs
+++ Database-Application-Teamwork/SupermarketChain/SupermarketChain.Data/Utilities/Constants.cs
@@ -11,5 +11,6 @@
         public const string PdfReportPath = @"C:\Users\Jinjaar-pc\Desktop\softuni-foxtail-fern\Database-Application-Teamwork\SupermarketChain\03. ExportToPdf\PdfReport\";
         public const string SalesImportPath = @"C:\Users\Jinjaar-pc\Desktop\softuni-foxtail-fern\Database-Application-Teamwork\SupermarketChain\02. ZipManager\Import\Sample-Sales-Reports.zip";
         public const string XmlExpensesPath = @"C:\Users\user\Desktop\softuni-foxtail-fern\Database-Application-Teamwork\SupermarketChain\06. LoadExpenseDataFromXml\Sample-Vendor-Expenses.xml";
+        public const string XmlReportsPath = @"C:\Users\Georgi\Desktop\asd\SupermarketChain\04. GenerateXmlReports\XmlReports\Sales-by-Vendors-Report.xml";
     }
 }
